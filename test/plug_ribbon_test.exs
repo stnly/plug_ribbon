e872170f9bf54769a0147ac797b35a82a3565aa2
Stanley Tan <hi@stnly.com>
2015-07-09 12:40:03 +0800

    Edit tests for consistency

--- test/plug_ribbon_test.exs
+++ test/plug_ribbon_test.exs
@@ -9,7 +9,7 @@ defmodule PlugRibbonTest do
 
   @ribbon_string ~s(<div class="plug-ribbon">)
 
-  test "injects banner in dev environment" do
+  test "injects ribbon in dev environment" do
     Mix.env(:dev)
     conn = conn(:get, "/")
     |> put_resp_content_type("text/html")
@@ -21,48 +21,48 @@ defmodule PlugRibbonTest do
     assert to_string(conn.resp_body) =~ @ribbon_string
   end
 
-  test "injects banner in test environment" do
+  test "injects ribbon in test environment" do
     Mix.env(:test)
     conn = conn(:get, "/")
     |> put_resp_content_type("text/html")
     |> resp(200, "<html><body><h1>Phoenix</h1></body></html>")
-    |> Plug.Ribbon.call([:test, :dev])
+    |> Plug.Ribbon.call([:dev, :test])
     |> send_resp()
 
     assert conn.status == 200
     assert to_string(conn.resp_body) =~ @ribbon_string
   end
 
-  test "does not inject banner in prod environment" do
+  test "does not inject ribbon in prod environment" do
     Mix.env(:prod)
     conn = conn(:get, "/")
     |> put_resp_content_type("text/html")
     |> resp(200, "<html><body><h1>Phoenix</h1></body></html>")
-    |> Plug.Ribbon.call([:test, :dev])
+    |> Plug.Ribbon.call([:dev, :test])
     |> send_resp()
 
     assert conn.status == 200
     refute to_string(conn.resp_body) =~ @ribbon_string
   end
 
-  test "does not inject banner if html response missing body tag" do
+  test "does not inject ribbon if html response missing body tag" do
     Mix.env(:dev)
     conn = conn(:get, "/")
     |> put_resp_content_type("text/html")
     |> resp(200, "<h1>Phoenix</h1>")
-    |> Plug.Ribbon.call([:test, :dev])
+    |> Plug.Ribbon.call([:dev, :test])
     |> send_resp()
 
     assert conn.status == 200
     refute to_string(conn.resp_body) =~ @ribbon_string
   end
 
-  test "does not inject banner if response is json" do
+  test "does not inject ribbon if response is json" do
     Mix.env(:dev)
     conn = conn(:get, "/")
     |> put_resp_content_type("application/json")
     |> resp(200, "{}")
-    |> Plug.Ribbon.call([:test, :dev])
+    |> Plug.Ribbon.call([:dev, :test])
     |> send_resp()
 
     assert conn.status == 200
